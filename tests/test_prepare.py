0291a96b50a038dfe05abb319ee8294cbd19e53f
tecnovert <tecnovert@particl.io>
2019-07-21 12:18:05 +0200

    Use importlib.import_module, undo symlink workaround.

--- tests/test_prepare.py
+++ tests/test_prepare.py
@@ -15,8 +15,9 @@ from io import StringIO
 from unittest.mock import patch
 import logging
 import shutil
+import importlib
 
-import bin.coldstakepool_prepare as prepareSystem
+prepareSystem = importlib.import_module('bin.coldstakepool-prepare')
 
 logger = logging.getLogger()
 logger.level = logging.DEBUG
