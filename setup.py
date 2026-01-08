88a0d79f0a770886c68d2353ef57e06c682d7739
tecnovert <tecnovert@tecnovert.net>
2026-01-08 23:35:34 +0200

    fix docker image

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python3",
+        "License :: MIT",
         "Operating System :: Linux",
     ],
     install_requires=[
